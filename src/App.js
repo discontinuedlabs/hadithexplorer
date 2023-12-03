b41a9a3d09dd97f53d213a686cd2288bb889fc11
Discontinued Labs <yu9811sef.ya@gmail.com>
2023-12-04 02:33:42 +0400

    basic css changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import SearchForm from "./components/SearchForm";
 export default function App() {
     return (
         <div className="App">
+            {/* <BooksSlider /> */}
+            {/* <input type="text"> */}
+            {/* <button>Clear</button> */}
             <SearchForm />
         </div>
     );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,29 +2,41 @@ import React, { useState, useEffect } from "react";
 
 export default function SearchForm() {
     const [searchTerm, setSearchTerm] = useState("");
-    const [data, setData] = useState(null);
+    const [ahadith, setAhadith] = useState([]);
 
     useEffect(() => {
         if (searchTerm) {
             fetch(`https://corsproxy.io/?https://dorar.net/dorar_api.json?skey=${searchTerm}`)
                 .then((response) => response.json())
-                .then((data) => setData(data))
+                .then((data) => {
+                    console.log(data);
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(data.ahadith.result, "text/html");
+                    const hadithElements = Array.from(doc.querySelectorAll(".hadith"));
+                    const hadithInfoElements = Array.from(doc.querySelectorAll(".hadith-info"));
+                    const ahadithArray = hadithElements.map((hadith, i) => ({
+                        hadith: hadith.innerHTML,
+                        hadithInfo: hadithInfoElements[i]?.innerHTML || "",
+                    }));
+                    setAhadith(ahadithArray);
+                })
                 .catch((error) => console.error(error));
         }
     }, [searchTerm]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Fetch data from the API here
-    };
-
     return (
         <div>
-            <form className="search-form" onSubmit={handleSubmit}>
-                <input type="text" onChange={(e) => setSearchTerm(e.target.value)}></input>
-                <button type="submit">Search</button>
-            </form>
-            {data && <div dangerouslySetInnerHTML={{ __html: data.ahadith.result }}></div>}
+            <input type="text" onChange={(e) => setSearchTerm(e.target.value)}></input>
+            {ahadith.length > 0 &&
+                ahadith.map((item) => {
+                    return (
+                        <div dir="rtl" className="hadith-box">
+                            <div className="hadith" dangerouslySetInnerHTML={{ __html: item.hadith }}></div>
+                            <div className="hadith-info" dangerouslySetInnerHTML={{ __html: item.hadithInfo }}></div>
+                        </div>
+                    );
+                })}
+            {ahadith.length > 0 && <button onClick={() => window.open(`https://dorar.net/hadith/search?q=${searchTerm}`, "_blank")}>More</button>}
         </div>
     );
 }
